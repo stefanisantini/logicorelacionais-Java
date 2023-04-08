daf1e586a539a049d4820018bec51e0b08dd8c12
Stéfani Santini Reis <110628639+stefanisantini@users.noreply.github.com>
2023-04-08 18:31:43 -0300

    Add files via upload

---
+++ LogicoRelacionais.java
@@ -0,0 +1,40 @@
+public class LogicoRelacionais {
+    public static void main(String[] args) throws Exception {
+        /*int n1, n2, r;
+        n1 = 2;
+        n2 = 8;
+        r = (n1>n2)?0:1; //o r resebeu 1 pq o n1 nao é maior q n2
+    System.out.println(r);*/
+
+    /**int n1, n2, r;
+    n1 = 14;
+    n2 = 18;
+    r = (n1>n2)?n1:n2; //vai mostar qual dos numeros é maior
+    System.out.println(r);*/
+
+    /**int n1, n2, r;
+    n1 = 14;
+    n2 = 8;
+    r = (n1>n2)?n1+n2:n1-n2; //se n1 for maior q n2 vou somar se n vou subtrair
+    System.out.println(r);*/
+
+    /**COMPARACAO DE STRING
+    String nome1 = "Stefani";
+    String nome2 = "Stefani";
+    String nome3 = new String("Stefani");
+    String res;
+    //res = (nome1==nome3)?"igual":"diferente";  o ultimo objeto foi instanciado com o new entao nao tem a mesma estrutura dos outros dois
+    res =(nome1.equals(nome3))?"igual":"diferente";//modo correto para objetos n usar== e sim equals
+    System.out.println(res);*/
+
+    /**OPERADOR LOGICO
+    int x, y, z;
+    x = 4;
+    y = 7;
+    z = 12;
+    boolean r;
+    r = (x<y && y<z)?true:false;
+    System.out.println(r);*/
+   
+    }
+}
